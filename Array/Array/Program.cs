0d302c0ae81b52b66c25e4913b435c76cd15abad
NijatS <nijatjs@code.edu.az>
2024-02-16 23:52:12 +0400

    DoWhileForEach

---
+++ Array/Array/Program.cs
@@ -0,0 +1,107 @@
+﻿
+#region 1-den 100-e qeder olan ededler arasinda 3-e ve ya 5-e bolunenleri cap ele.
+//for (int i = 0; i < 100;i++)
+//{
+//	if (i%3==0||i%5==0)
+//	{
+//        Console.WriteLine(	i);
+
+//    }
+//}
+#endregion
+
+#region for islederek -50 den 150-e qeder olan ededlerin arasinda 5-e bolunenlerin cemini cap ele.
+//int cem = 0;
+//int say = 0;
+//for (int i = -50; i<= 150; i++)
+//{
+//	if (i%5==0)
+//	{
+//		cem =cem + i;//hemin araliqdaki 5-e bolunen ededlerin cemi
+//		say = say + 1; //hemin araliqdaki 5-e bolunen ededlerin sayi
+//    }
+//}
+//Console.WriteLine("ededlerin cemi" + cem	);
+//Console.WriteLine("ededlerin sayi"+say	);
+
+//string a = "Ni";
+//string b = "jat";
+//Console.WriteLine(a + b);
+#endregion
+
+#region while islederek userden bir qiymet alirsan.eger user 5 den boyuk eded daxil ederse dongu sonlansin, eks halda yeniden istesin
+//while (true)
+//{
+//    Console.WriteLine("zehmetolmasa eded daxil edin"  );
+//   int eded=int.Parse(Console.ReadLine());
+//	if (eded>5)
+//	{
+//		break;
+//	}
+//}
+#endregion
+
+#region DoWhile
+
+//int i = 0;
+
+//while (i < 0)
+//{
+//	i++;
+//}
+//Console.WriteLine(i);
+
+//do
+//{
+//	i++;
+//}
+//while (i < 0);
+
+//Console.WriteLine(i);
+
+#endregion
+
+#region 5den 50ye kimi ededlerin cemi
+//int cem = 0;
+//int i = 5;
+//do
+//{
+
+//	cem = cem + i;
+//	i++;
+//}
+
+//while (i<50);
+//Console.WriteLine("ededlerin cemi" + cem);
+#endregion
+
+#region ForEach
+//int[] ededler = { 4, 6, 7, 9, 10 };
+
+//for (int i = 0; i < ededler.Length;i++)
+//{
+//    Console.WriteLine(ededler[i]);
+//}
+
+//foreach (int eded in ededler)
+//{
+//    Console.WriteLine(eded);
+//}
+
+//Console.WriteLine(ededler[3]);
+#endregion
+
+#region ForEachTask
+
+//string[] adlar = { "samir", "rsul", "mahir" };
+//for (int i = 0; i < adlar.Length; i++)
+//{
+//    Console.WriteLine(adlar[i]);
+
+//}
+//foreach (string ad in adlar)
+//{
+//    Console.WriteLine(ad  );
+//}
+
+#endregion

5308c2ffb603d855e52f1742d233902e5fe533f0
NijatS <nijatjs@code.edu.az>
2024-02-28 23:30:10 +0400

    metodlar elave olundu

---
+++ Methods/MethodTest/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace MethodTest
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+            //Toplama(20,30);
+            Console.WriteLine("a deyerini daxil edin"	);
+
+            int a=int.Parse(Console.ReadLine());
+            Console.WriteLine(	"b deyerini daxil edin");
+
+            int b=int.Parse(Console.ReadLine());
+
+			int cem = Toplama(a, b);
+            Console.WriteLine("Ededlerin cemi "+ cem);
+           int vurma= Vurma(a, b);
+            Console.WriteLine(	"ededlerin vurulmasi" +vurma);
+           int cixma= Cixma(a, b);
+            Console.WriteLine(	"ededlerin cixmasi"+cixma);
+             float bolme=Bolme(a, b);
+            Console.WriteLine(	"ededlerin bolunmesi"+ bolme);
+        }
+
+
+		//public static void Toplama(int a, int b)
+		//{
+		//	int toplam = a+ b;
+		//	Console.WriteLine("ededlerin toplanmasi " +toplam);
+  //      }
+		public static int Toplama(int a, int b)
+		{
+			int toplam = a + b;
+			return toplam;
+		}
+		//public static void Vurma(int a, int b) {
+
+		//	int vurma = a * b;
+  //          Console.WriteLine(	"ededlerin hasili " +vurma);
+  //      }
+		public static int Vurma(int a, int b)
+		{
+			int vurma = a * b;
+			return vurma;
+		}
+		//public static void Cixma(int a,int b) {
+		//	int cixma = a - b;
+		//          Console.WriteLine("ededlerin cixilmasi " +cixma);
+		//      }
+		public static int Cixma(int a, int b)
+		{
+			int cixma = a - b;
+			return cixma;	
+		}
+
+		//public static void Bolme(int a,int b)
+		//{
+		//	float bolme =(float) a / b;
+		//          Console.WriteLine("ededlerin bolunmesi "+ bolme	);
+		//      }
+		public static float Bolme(int a, int b)
+		{
+			float bolme = (float)a / b;
+			return bolme;	
+			
+		}
+
+
+
+
+
+	}
+}
+
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Methods
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Hello();
+			//Hello();
+			//Hello();
+			//Hello();
+			//Hello();
+
+
+			//Hello("samir");
+			//Hello("nicat");
+			Console.WriteLine("zehmet olmasa deyer girin"	);
+			int a=int.Parse(Console.ReadLine());
+
+			//int kvadrat = a * a;
+			//Console.WriteLine( kvadrat );
+
+			int cavab = kvadrati(a);
+            Console.WriteLine(cavab);
+            //Toplama(10,11);
+
+
+
+            //int a1 = 15;
+            //int b1 = 15;
+            //Cixma(a1,b1);
+
+
+            //List<int> ints = new List<int>();
+            //ints.Add(1);
+            //ints.Add(2);
+
+            //int listCem = ints.Sum();
+            //         Console.WriteLine();
+
+
+
+
+        }
+
+
+		public static void Toplama(int a , int b)
+		{
+			int cem = a + b;
+            Console.WriteLine("Cem: " + cem);
+        }
+
+		public static void Cixma(int a , int b) {
+
+			int cixma = a - b;
+            Console.WriteLine("cixma"+cixma	);
+        }
+
+		public static void Hello()
+		{
+			Console.WriteLine("Salam Samir");
+		}
+
+		public static void Hello(string ad)
+		{
+			Console.WriteLine("Salam "+ad);
+		}
+		//public static void kvadrati(int a)
+		//{
+		//	int kvadrat = a * a;
+  //          Console.WriteLine("kvadrati" + kvadrat	);
+  //      }
+		public static int kvadrati(int a)
+		{
+			int kvadrat = a * a;
+			return kvadrat;
+		}
+
+	}
+}

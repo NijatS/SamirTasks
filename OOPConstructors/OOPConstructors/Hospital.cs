cd14b0393f7ba667dd72ba5062302ebc45b17c1f
NijatS <nijatjs@code.edu.az>
2024-05-01 02:12:18 +0400

    add:OOPConstructors

---
+++ OOPConstructors/OOPConstructors/Hospital.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConstructors
+{
+	public class Hospital
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public string Phone {  get; set; }
+
+		public Hospital() {
+            Console.WriteLine("Constructor isledi");
+        }
+
+		private void Analiz()
+		{
+            Console.WriteLine("Analiz Methodu isledi");
+        }
+
+		public int AnalizCavablari()
+		{
+			Console.Write("Ilk analiz cavabinizi daxil edin:");
+			int analiz1 = int.Parse(Console.ReadLine());
+
+			Console.Write("Ikinci analiz cavabinizi daxil edin:");
+			int analiz2 = int.Parse(Console.ReadLine());
+
+			int edediOrta = (analiz1+ analiz2)/2;
+			return edediOrta;
+		}
+
+		public string GetNameAddress()
+		{
+			string full_ad = Name + " " + Address;
+			return full_ad;
+		}
+
+
+
+		//[access modifier]   [return type]    [adi](paramertler)
+		//public prive inter  void int string  Ne is gorurse ona uygun ad veririk
+		//protected
+	}
+}
---
+++ OOPConstructors/OOPConstructors/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace OOPConstructors
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Hospital hospital1 = new Hospital();
+			hospital1.Id = 1;
+			hospital1.Name = "Medical";
+			hospital1.Address = "Babek";
+			hospital1.Phone = "1248421";
+
+			Hospital hospital2 = new Hospital();
+			hospital2.Id = 2;
+			hospital2.Name = "Medical";
+			hospital2.Address = "28 May";
+			hospital2.Phone = "232425";
+
+			//Console.WriteLine(hospital1.Name);
+
+
+			hospital1.Analiz();
+			//int cavab = hospital1.AnalizCavablari();
+			//Console.WriteLine(cavab);
+
+			Console.WriteLine(hospital1.GetNameAddress());
+			Console.WriteLine(hospital2.GetNameAddress());
+		}
+	}
+}

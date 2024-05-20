b3fc8070b8eb40337579b278c77fd399e0840911
NijatS <nijatjs@code.edu.az>
2024-05-21 02:40:30 +0400

    review:WhileDoWhile

---
+++ WhileDoWhile/WhileDoWhile/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace WhileDoWhile
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//int i = 0;
+			//while(i > 10)
+			//{
+			//             Console.WriteLine(i);
+			//	i++;
+			//         }
+			//         Console.WriteLine("\n------------------------------------\n");
+
+			//         int j = 0;
+			//do
+			//{
+			//	Console.WriteLine(j);
+			//	j++;
+			//}
+			//while (j > 10);
+
+
+			bool DavamEtsinmi = true;
+			do
+			{
+				Console.WriteLine("Eded daxil edin:");
+				int eded = int.Parse(Console.ReadLine());
+
+				if (eded % 3 == 0 && eded % 5 == 0)
+				{
+                    Console.WriteLine(eded);
+					DavamEtsinmi = false;
+                }
+            }
+			while (DavamEtsinmi);
+
+
+
+
+        }
+	}
+}

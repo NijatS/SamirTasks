ae34231b77abbb3a04e24ce2caec558781f58525
NijatS <nijatjs@code.edu.az>
2024-05-27 01:16:01 +0400

    add:ArrayListReview

---
+++ ArrayListReview/ArrayListReview/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+
+namespace ArrayListReview
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//int[] ededler = { 1, 2, 7, 4, 5 };
+
+			//ededler[5] = 11;
+
+
+			//for (int i = 0; i < ededler.Length; i++)
+			//{
+			//	Console.WriteLine(ededler[i]);
+			//}
+
+
+			ArrayList arrayList = new ArrayList();
+			arrayList.Add(1);
+			arrayList.Add(12);
+			arrayList.Add(3);
+			arrayList.Add(-4);
+
+			//for (int i = 0; i < arrayList.Count; i++) {
+			//	Console.WriteLine(arrayList[i]);
+			//         }
+			Console.WriteLine("------------------------");
+			arrayList.Add(101);
+			arrayList.Add("Samir");//Sonuna elave eliyir
+			arrayList.Insert(2, "Nijat");//Verdiyimiz indexe elave edir
+
+			arrayList.Remove("Samir");//Verdiyimi deyeri silir
+			arrayList.RemoveAt(2);//Verdiyimiz indexe uygun silir
+
+			arrayList.Sort();
+
+			//for (int i = 0; i < arrayList.Count; i++)
+			//{
+			//	Console.WriteLine(arrayList[i]);
+			//}
+
+
+			List<int> ededler = new List<int>();
+			ededler.Add(1);
+			ededler.Add(2);
+			ededler.Add(3);
+			//ededler.Add("Samir");//Compile Time Error
+
+			foreach (int eded in ededler)
+			{
+				Console.WriteLine(eded);
+			}
+
+			List<string> adlar = new List<string>();
+			adlar.Add("Samir");
+			adlar.Add("Nijat");
+			adlar.Add("Test");
+
+			Console.WriteLine("----------------------------");
+			adlar.Sort();//Elifba sirasi ile cixarir
+			adlar.Reverse(); // Verilen listin tersine cixarir
+
+			foreach (string ad in adlar)
+			{
+				Console.WriteLine(ad);
+			}
+			int a = 1;
+			int b = 10;
+			//For
+			//for (int i = a; i <= b; i++)
+			//{
+			//             Console.WriteLine(i);
+			//}
+
+			//While
+			int i = a;
+			//while (i<=b)
+			//{
+			//             Console.WriteLine(i);
+			//	i++;
+			//         }
+
+			//DoWhile
+			do
+			{
+				Console.WriteLine(i);
+				i++;
+			}
+			while (i <= b);
+
+
+		}
+	}
+}
---
+++ Test/Test/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Test
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//int[] ededler = {-5,2,6,-6,7,8,-9,11,20};
+			//int musbetSay = 0;
+			//int menfiSay = 0;
+
+			//for (int i = 0; i < ededler.Length; i++)
+			//{
+			//	if (ededler[i] > 0)
+			//	{
+			//		musbetSay++;
+			//	}
+			//	else
+			//	{
+			//		menfiSay++;
+			//	}
+			//}
+			//         Console.WriteLine("Musbet say:" + musbetSay);
+			//Console.WriteLine("Menfi say:" + menfiSay);
+
+
+			//              ededler[0]  ededler[1]                   ededler[5]
+			//int[] ededler = {  -5   ,       32    ,    -23, 41, 55,      62    };
+			//int cem = 0;
+
+
+			//for (int i = 0; i < ededler.Length; i++)
+			//{
+			//	cem += ededler[i];
+			//}
+			//Console.WriteLine("Cem:" + cem);
+
+
+
+
+			int[] ededler = { 1, 11, 51, 5, -8, 32, 62, -15, 44 };
+			int min = ededler[0];
+
+			for (int i = 1; i < ededler.Length; i++)
+			{
+				if (ededler[i] < min)
+				{
+					min = ededler[i];
+				}
+			}
+            Console.WriteLine("Min:" + min);
+
+
+        }
+	}
+}
